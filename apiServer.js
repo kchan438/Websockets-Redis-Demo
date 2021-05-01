4b5e89392e56f7e6b66c7d08b52ae5a8307253e0
Kevin Chan <kevinchan128@msn.com>
2021-04-30 18:16:48 -0700

    Commit 1
    Passed api.test, edited websocket2 test path, and added done() at line 42.

--- apiServer.js
+++ apiServer.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const redis = require('redis');
 const app = express();
+const redisClient = redis.createClient();
 
 
 // http://localhost:4000/api/doSomething?userId=abc&text=hello
+
+app.get('/api/doSomething', (req, res) => {
+  console.log('Got message.');
+  const redisData = {
+    type: 'USER_SENT_MESSAGE',
+    userId: req.query.userId,
+    text: req.query.text,
+  };
+  // console.log('userID: ' + redisData.userId);
+  redisClient.publish('testPublish', JSON.stringify(redisData));
+  redisClient.publish('testPublish', JSON.stringify(redisData));
+  res.status(200).send(redisData);
+});
 
 module.exports = app;
 
