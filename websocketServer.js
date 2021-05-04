9ac036e22de37ed58973fb09c9c0321c376fde95
Kevin Chan <kevinchan128@msn.com>
2021-05-03 17:01:15 -0700

    Commit 3
    Fixed lint indentation errors.

--- websocketServer.js
+++ websocketServer.js
@@ -10,22 +10,22 @@ const chatMessages = [];
 
 //sends a message to all clients
 const broadcast = (obj) => {
-    wss.clients.forEach((client) => {
-        client.send(JSON.stringify(obj));
-    });
+  wss.clients.forEach((client) => {
+    client.send(JSON.stringify(obj));
+  });
 };
 
 redisClient.on('message', (channel, message) => {
-    // console.log(`Message on channel: ${channel} is ${message}`);
-    const messageJSON = JSON.parse(message);
-    // console.log('JSON: ' + messageJSON.userId);
-    //messages each client if the client id and the message id match
-    wss.clients.forEach((client) => {
-        // console.log('client: ' + client.userId);
-        if(client.userId === messageJSON.userId) {
-            client.send(message);
-        }
-    });
+  // console.log(`Message on channel: ${channel} is ${message}`);
+  const messageJSON = JSON.parse(message);
+  // console.log('JSON: ' + messageJSON.userId);
+  //messages each client if the client id and the message id match
+  wss.clients.forEach((client) => {
+    // console.log('client: ' + client.userId);
+    if(client.userId === messageJSON.userId) {
+      client.send(message);
+    }
+  });
 });
     
 redisClient.subscribe('testPublish');
@@ -33,16 +33,16 @@ redisClient.subscribe('testPublish');
 //triggers when a client connects
 //saves userId and sends messages to everyone who connects
 wss.on('connection', (ws) => {
-    console.log('Client has connected.');
-    //event handler when a message is received
-    //saves message into chatMessages array 
-    ws.on('message', (data) => {
-        const dataID = JSON.parse(data);
-        console.log(dataID)
-        chatMessages.push(dataID);
-        broadcast({
-            type: 'UPDATE_CHAT',
-            chatMessages: chatMessages,
-        });
+  console.log('Client has connected.');
+  //event handler when a message is received
+  //saves message into chatMessages array 
+  ws.on('message', (data) => {
+    const dataID = JSON.parse(data);
+    console.log(dataID)
+    chatMessages.push(dataID);
+    broadcast({
+      type: 'UPDATE_CHAT',
+      chatMessages: chatMessages,
     });
+  });
 });
